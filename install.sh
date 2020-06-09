6999d412e767c56217100a0aa9c21a32d128aefc
nlaaziz <contact@naoufal-laaziz.me>
2020-06-10 00:32:41 +0200

    add install file and fix some issues

---
+++ install.sh
@@ -0,0 +1,5 @@
+oc create -f deploy/crds/cleaning.ansible-operator.com_cleaningconfigs_crd.yaml
+oc create -f deploy/service_account.yaml
+oc create -f deploy/role.yaml
+oc create -f deploy/role_buiding.yaml
+oc create -f deploy/operator.yaml
