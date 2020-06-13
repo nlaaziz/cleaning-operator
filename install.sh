94e2bf6d79d10db59d69841459917c2674424d74
nlaaziz <contact@naoufal-laaziz.me>
2020-06-13 02:11:23 +0200

    add ephemral namespace

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
-oc create -f deploy/crds/cleaning.ansible-operator.com_cleaningconfigs_crd.yaml
-oc create -f deploy/service_account.yaml
-oc create -f deploy/role.yaml
-oc create -f deploy/role_binding.yaml
-oc create -f deploy/operator.yaml
+export NS_OPERATOR=cleaning-operator
+echo "operator namespace is" $NS_OPERATOR
+oc create namespace $NS_OPERATOR
+oc create -f deploy/crds/cleaning.ansible-operator.com_cleaningconfigs_crd.yaml -n $NS_OPERATOR
+oc create -f deploy/crds/ephemeral-namespace-crd.yaml -n $NS_OPERATOR
+oc create -f deploy/service_account.yaml -n $NS_OPERATOR
+oc create -f deploy/clusterrole.yaml -n $NS_OPERATOR
+oc create -f deploy/clusterrolebinding.yaml -n $NS_OPERATOR
+oc create -f deploy/operator.yaml -n $NS_OPERATOR
