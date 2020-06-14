594e3adb579d3e0b027eab4a836aa137605adeaa
nlaaziz <contact@naoufal-laaziz.me>
2020-06-14 03:15:56 +0200

    fix generated schedule time

--- roles/ephemeral_namespace_watch/files/generate_schedule_param.py
+++ roles/ephemeral_namespace_watch/files/generate_schedule_param.py
@@ -5,4 +5,4 @@ namespace_creation_date = datetime.strptime(sys.argv[1], '%Y-%m-%dT%H:%M:%SZ')
 namespace_exiry = sys.argv[2]
 end_date = namespace_creation_date + timedelta(days=int(namespace_exiry))
 
-print(end_date.strftime('%M %H %d %m %d'))
+print(end_date.strftime('%M %H %d %m *'))
